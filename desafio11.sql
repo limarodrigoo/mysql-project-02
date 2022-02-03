935f75a680008316acea961d781ab63819d59c9d
Rodrigo Lima <limarodrigoo@outlook.com>
2022-02-03 20:16:18 -0300

    :bento: add query change music names

---
+++ desafio11.sql
@@ -0,0 +1,19 @@
+SELECT 
+    name AS nome_musica, 
+    CASE
+		WHEN name LIKE '%Street%' THEN REPLACE(name, 'Streets', 'Code Review')
+        WHEN name LIKE '%Her Own' THEN REPLACE(name, 'Her Own','Trybe')
+        WHEN name LIKE '%Inner Fire%' THEN REPLACE(name, 'Inner Fire', 'Project')
+        WHEN name LIKE '%Silly%' THEN REPLACE(name, 'Silly','Nice')
+        WHEN name LIKE '%Circus%' THEN REPLACE(name, 'Circus', 'Pull Request')
+        ELSE name
+        END AS novo_nome
+FROM
+    SpotifyClone.music
+WHERE
+    name LIKE '%Street%'
+        OR name LIKE '%Her Own%'
+        OR name LIKE '%Inner Fire%'
+        OR name LIKE '%Silly%'
+        OR name LIKE '%Circus%'
+ORDER BY name;
